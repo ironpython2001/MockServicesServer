58b30bb383accddd94c50d026aba1be239750782
ironpython2001 <ironpython2001@gmail.com>
2014-04-09 07:23:52 +0530

    Initial version of MockServices

---
+++ MockServices.Server/MockServices.Server/MockServicesServerApplication.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fiddler;
+
+namespace MockServices.Server
+{
+    public interface IMockServicesServerApplication
+    {
+
+        void Start(int portNo);
+
+        void Shutdown();
+
+    }
+
+    public class MockServicesServerApplication:IMockServicesServerApplication
+    {
+
+        public MockServicesServerApplication()
+        {
+            FiddlerApplication.BeforeRequest += new SessionStateHandler(FiddlerApplication_BeforeRequest);
+            FiddlerApplication.BeforeResponse += new SessionStateHandler(FiddlerApplication_BeforeResponse);
+        }
+
+
+        public void Start(int portNo)
+        {
+            FiddlerApplication.Startup(portNo, FiddlerCoreStartupFlags.Default);
+            Console.WriteLine("Started Server..");
+
+        }
+
+
+        public void Shutdown()
+        {
+            FiddlerApplication.Shutdown();
+        }
+
+
+        static void FiddlerApplication_BeforeRequest(Session oSession)
+        {
+            //FiddlerApplication.Log.LogFormat(oSession.fullUrl);
+            Console.WriteLine(oSession.fullUrl);
+
+        }
+
+
+        static void FiddlerApplication_BeforeResponse(Session oSession)
+        {
+            oSession.utilSetResponseBody("asdfasdF");
+            Console.WriteLine(oSession.GetResponseBodyAsString());
+
+        }
+
+    }
+}
